72b7f9dd1ef9d10c79ecb3bba1e17bac7731aaf6
Janmnik <pyjamaman@janmnik.pl>
2022-06-25 16:05:24 +0200

    added exercise about list

---
+++ ex34.py
@@ -0,0 +1,32 @@
+#uzyskanie dostępu do elementów list
+
+#lista
+animals = ['niedźwiedź', 'tygrys', 'pingwin', 'zebra']
+bear = animals[0]
+animals=['niedźwiedż', 'python', 'paw','kangur','dziobak']
+
+
+print("1. Zwierzę z pozycji 1.")
+one=animals[1]
+print(one)
+print("2. Trzecie zwierzę.")
+two=animals[2]
+print(two)
+print("3. Pierwsze zwierzę.")
+three=animals[0]
+print(three)
+print("4. Zwierzę z pozycji 3.")
+four=animals[3]
+print(three)
+print("5. Piąte zwierzę.")
+five=animals[4]
+print(five)
+print("6. Zwierzę z pozycji 2.")
+six=animals[2]
+print(six)
+print("7. Szóste zwierzę.")
+seven=animals[5]
+print(seven)
+print("8. Zwierzę z pozycji 4.")
+eight=animals[4]
+print(eight)
